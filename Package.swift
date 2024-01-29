896e3c3231966ce804a9e53676096ebaead02480
MAHESHWARAN <maheshwaran99m@gmail.com>
2024-01-29 07:43:51 +0530

    updated platform

--- Package.swift
+++ Package.swift
@@ -5,7 +5,6 @@ import PackageDescription
 
 let package = Package(
   name: "LayoutConstraint",
-  platforms: [.iOS(.v13), .macOS(.v10_15)],
   products: [
     // Products define the executables and libraries a package produces, making them visible to other packages.
     .library(
